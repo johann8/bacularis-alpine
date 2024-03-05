cac372ae1f5e87c267666b0bd4c586abb71ca5bb
johann8 <jo.hahn@gmx.de>
2024-03-05 09:56:46 +0100

    changed script flow

--- 2_create_new_bacula_client_linux--client_side_template.sh
+++ 2_create_new_bacula_client_linux--client_side_template.sh
@@ -5,12 +5,12 @@
 #
 
 # Set variables
-#MD5_PASSWORD=                                                                    #  On bacula server: cat bacula-dir.conf ( Client {  Name = "oraclel8-fd" Password = "[md5]a1e10524f1a33eae3496d1104b056f3e"} )
-SCRIPT_VERSION=0.3
-DIRECTOR_ADDRESS=192.168.15.16                                                    # IP Address of docker host wher bacula server is running
-DIRECTOR_NAME=bacula-dir                                                          # The name of bacula server director (bacula-dir)
-DIRECTOR_CONSOLE_MONITOR_NAME=bacula-mon                                          # The name of bacula server console (bacula-mon)
-DIRECTOR_CONSOLE_MONITOR_PASSWORD='Vy1holhTDZ3xPYB6s0QaqW26/1levNlVNqU07i+rLQUt'  # On bacula server: cat bacula-dir.conf ( Console {Name = "bacula-mon" Password = "Vy1holhTDZ3xPYB6s0QaqW26/1levNlVNqU07i+rLQUt" } )
+SCRIPT_VERSION=0.4
+DIRECTOR_ADDRESS=192.168.15.16                                         # IP Address of docker host wher bacula server is running
+CLIENT_INSTALL_DIR="/opt/bacula/etc"                                   # Bacula client install dir
+DIRECTOR_NAME=bacula-dir                                               # The name of bacula server director (bacula-dir)
+DIRECTOR_CONSOLE_MONITOR_NAME=bacula-mon                               # The name of bacula server console (bacula-mon)
+DIRECTOR_CONSOLE_MONITOR_PASSWORD='###BACULA_DIR_MON_PASSWORD###'      # On bacula server: cat bacula-dir.conf ( Console {Name = "bacula-mon" Password = "Vy1holhTDZ3xPYB6s0QaqW26/1levNlVNqU07i+rLQUt" } )
 
 # Function
 show_help() {
@@ -91,19 +91,7 @@ BCONSOLE_CONFIG_FILE=bconsole_${CLIENT_NAME}.conf
 CONFIG_FOLDER=config_files
 MD5_PASSWORD=$(cat ${CONFIG_FOLDER}/bacula-dir_${CLIENT_NAME}.conf |grep Password |cut -f 2 -d '"')
 
-# Create config files
-if [[ -f /opt/bacula/etc/bacula-fd.conf ]]; then
-   echo -n "Creating backup of \"bacula-fd.conf\"..."
-   cp /opt/bacula/etc/bacula-fd.conf /opt/bacula/etc/bacula-fd.conf.backup
-   echo [DONE]
-fi
-
-if [[ -f /opt/bacula/etc/bconsole.conf ]]; then
-   echo -n "Creating backup of \"bconsole.conf\"..."
-   cp /opt/bacula/etc/bconsole.conf /opt/bacula/etc/bconsole.conf.backup
-   echo [DONE]
-fi
-
+### create folder for config files
 if [[ ! -d ${CONFIG_FOLDER} ]]; then
    echo -n "Creating folder \"${CONFIG_FOLDER}\"...                       "
    mkdir ${CONFIG_FOLDER}
@@ -133,75 +121,30 @@ if [[ -f ${BACULA_FD_CONFIG_FILE_TEMPLATE} ]] && [[ -f ${BCONSOLE_CONFIG_FILE_TE
              -e "s/###DIRECTOR_CONSOLE_MONITOR_NAME###/${DIRECTOR_CONSOLE_MONITOR_NAME}/" \
              -e "s!###DIRECTOR_CONSOLE_MONITOR_PASSWORD###!${DIRECTOR_CONSOLE_MONITOR_PASSWORD}!" ${CONFIG_FOLDER}/${BACULA_FD_CONFIG_FILE}
       echo [DONE]
-   
+
       echo -n "Inserting variables into \"bconsole.conf\" file...        "
       sed -i -e "s/###DIRECTOR_NAME###/${DIRECTOR_NAME}/" \
              -e "s/###DIRECTOR_ADDRESS###/${DIRECTOR_ADDRESS}/" \
              -e "s/###MD5_PASSWORD###/${MD5_PASSWORD}/" ${CONFIG_FOLDER}/${BCONSOLE_CONFIG_FILE}
-      echo [DONE]  
+      echo [DONE]
    fi
 fi
 
 exit 0
 
-cat > bacula-fd_template.conf << 'EOL'
-# cat /opt/bacula/etc/bacula-fd.conf
-#
-# Default  Bacula File Daemon Configuration file
-#
+### Only for bacula client on docker host
+# copy files to client install dir
+if [[ -d ${CLIENT_INSTALL_DIR} ]]; then
+   echo -n "Copying \"bacula-fd.conf\" file...    "
+   \cp ${CONFIG_FOLDER}/${BACULA_FD_CONFIG_FILE} ${CLIENT_INSTALL_DIR}/bacula-fd.conf
+   echo [DONE]
 
-#
-# List Directors who are permitted to contact this File daemon
-#
-Director {
-  Name = ###DIRECTOR_NAME###
-  Password = "###MD5_PASSWORD###"
-}
+   echo -n "Copying \"bconsole.conf\" file...    "
+   \cp ${CONFIG_FOLDER}/${BCONSOLE_CONFIG_FILE} ${CLIENT_INSTALL_DIR}/bconsole.conf
+   echo [DONE]
+fi
 
-#
-# Restricted Director, used by tray-monitor to get the
-#   status of the file daemon
-#
-Director {
-  Name = ###DIRECTOR_CONSOLE_MONITOR_NAME###
-  Password = "###DIRECTOR_CONSOLE_MONITOR_PASSWORD###"
-  Monitor = yes
-}
-
-#
-# "Global" File daemon configuration specifications
-#
-FileDaemon {                               # this is me
-  Name = ###CLIENT_NAME###
-  FDport = 9102                             # where we listen for the director
-  WorkingDirectory = /opt/bacula/working
-  Pid Directory = /opt/bacula/working
-  Maximum Concurrent Jobs = 20
-  Plugin Directory = /opt/bacula/plugins
-}
-
-# Send all messages except skipped files back to Director
-Messages {
-  Name = Standard
-  director = ###DIRECTOR_NAME### = all, !skipped, !restored, !verified, !saved
-}
-EOL
-
-cat > bconsole_template.conf << 'EOL'
-# cat /opt/bacula/etc/bconsole.conf
-#
-# Bacula User Agent (or Console) Configuration File
-#
-
-Director {
-  Name = ###DIRECTOR_NAME###
-  DIRport = 9101
-  address = ###DIRECTOR_ADDRESS###
-  Password = "###MD5_PASSWORD###"
-}
-EOL
-
-# Unter Windows
-Director Name: bacula-dir                                    # Bacula server - Director Name
-Director Password: [md5]ecd4dc823c8699998de286ed5183b75e     # Run under linux: PASSWORD="$(pwgen 32 1)"; MD5_PASSWORD=$(echo -n "$PASSWORD"|md5sum| sed -r 's/\s+.*$//;s/^/[md5]/'); echo "Password: ${PASSWORD}"; echo "MD5 Password: ${MD5_PASSWORD}"
-Director Address: 192.168.15.16                              # ip a sh
+### Unter Windows
+# Director Name: bacula-dir                                    # Bacula server - Director Name
+# Director Password: [md5]ecd4dc823c8699998de286ed5183b75e     # Run under linux: PASSWORD="$(pwgen 32 1)"; MD5_PASSWORD=$(echo -n "$PASSWORD"|md5sum| sed -r 's/\s+.*$//;s/^/[md5]/'); echo "Password: ${PASSWORD}"; echo "MD5 Password: ${MD5_PASSWORD}"
+# Director Address: 192.168.15.16                              # ip a sh

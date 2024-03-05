8802e3091df70f7915517847a40a9591d21986c8
johann8 <jo.hahn@gmx.de>
2024-03-05 09:54:44 +0100

    changed script flow

--- 1_create_new_bacula_client_linux--server_side_template.sh
+++ 1_create_new_bacula_client_linux--server_side_template.sh
@@ -5,11 +5,16 @@
 #
 
 # Set variables
-SCRIPT_VERSION=0.3
+SCRIPT_VERSION=0.4
 PASSWORD="$(pwgen 32 1)"
 MD5_PASSWORD=$(echo -n "$PASSWORD"|md5sum| sed -r 's/\s+.*$//;s/^/[md5]/')
 CLIENT_NAME=
 IP_ADDRESS=
+BACULA_SERVER_CONFIG_DIR_DOCKER="/opt/bacularis/data/bacula/config/etc/bacula/bacula-dir.conf"
+SCRIPT_NAME_CLIENT="2_create_new_bacula_client_linux--client_side_template.sh"
+BACULA_DIR_CONFIG_FILE_TEMPLATE="bacula-dir_template.conf"
+CONFIG_FOLDER="config_files"
+
 
 # Function
 show_help() {
@@ -97,9 +102,7 @@ fi
 # Pass variables
 CLIENT_NAME=${BACULA_CLIENT_NAME}
 IP_ADDRESS=${BACULA_CLIENT_IP_ADDRESS}
-BACULA_DIR_CONFIG_FILE_TEMPLATE=bacula-dir_template.conf
 BACULA_DIR_CONFIG_FILE=bacula-dir_${CLIENT_NAME}.conf
-CONFIG_FOLDER=config_files
 
 # create config folder
 if [[ ! -d ${CONFIG_FOLDER} ]]; then
@@ -131,81 +134,20 @@ if [[ -f ${BACULA_DIR_CONFIG_FILE_TEMPLATE} ]]; then
    fi
 fi
 
-exit 0
+# read bacula-mon password
+WORKING_DIR=$(pwd)
+if [[ -f ${WORKING_DIR}/${SCRIPT_NAME_CLIENT} ]]; then
+   # read bacula-dir monitor pw
+   echo -n "Reading bacula-dir monitor pw..."
+   BACULA_DIR_MON_PW=$(cat ${BACULA_SERVER_CONFIG_DIR_DOCKER} |sed -n '/bacula-mon/,+1p' |grep Password |cut -f 2 -d '"')
+   echo [DONE]
 
-# Template
-cat > bacula-dir_template.conf << 'EOL'
-Client {
-  Name = "###CLIENT_NAME###-fd"
-  Address = "###IP_ADDRESS###"
-  FdPort = 9102
-  Password = "###MD5_PASSWORD###"
-  Description = "Backup Host - ###CLIENT_NAME###"
-  Catalog = "MyCatalog"
-  FileRetention = 5184000
-  JobRetention = 15552000
-  AutoPrune = yes
-}
-Job {
-  Name = "backup-###CLIENT_NAME###"
-  JobDefs = "###CLIENT_NAME###-job"
-  Description = "Backup Host - ###CLIENT_NAME###"
-}
-JobDefs {
-  Name = "###CLIENT_NAME###-job"
-  Type = "Backup"
-  Level = "Incremental"
-  Messages = "Standard"
-  Storage = "File1"
-  Pool = "Incremental"
-  FullBackupPool = "Full"
-  IncrementalBackupPool = "Incremental"
-  DifferentialBackupPool = "Differential"
-  Client = "###CLIENT_NAME###-fd"
-  Fileset = "###CLIENT_NAME###-fset"
-  Schedule = "WeeklyCycle"
-  WriteBootstrap = "/var/lib/bacula/%c.bsr"
-  SpoolAttributes = yes
-  Priority = 10
-}
-Fileset {
-  Name = "###CLIENT_NAME###-fset"
-  Include {
-    File = "/etc"
-    File = "/opt/bacula/etc"
-    Options {
-      Signature = MD5
-      Wild = *.jpg
-      Wild = *.png
-      Wild = *.gif
-      Wild = *.zip
-      Wild = *.rar
-      Wild = *.7z
-      Wild = *.r??
-      Wild = *.mpg
-      Wild = *.wmv
-      Wild = *.avi
-      Wild = *.mov
-      Wild = *.mkv
-      Wild = *.mp3
-      Wild = *.mp4
-      Wild = *.gz
-      Wild = *.bz2
-    }
-    Options {
-      Signature = "Md5"
-      Compression = "LZO"
-    }
-  }
-  Exclude {
-    File = "/proc"
-    File = "/tmp"
-    File = "/sys"
-    File = "/.journal"
-    File = "/.fsck"
-    File = "/etc/selinux"
-    File = "/etc/udev"
-  }
-}
-EOL
+   # insert passwort in script
+   echo -n "Inserting password to script... "
+   sed -i -e "s/###BACULA_DIR_MON_PASSWORD###/${BACULA_DIR_MON_PW}/" ${WORKING_DIR}/${SCRIPT_NAME_CLIENT}
+   echo [DONE]
+else
+   echo "Script \"${WORKING_DIR}/${SCRIPT_NAME_CLIENT}\" could not be find."
+fi
+exit 0
 

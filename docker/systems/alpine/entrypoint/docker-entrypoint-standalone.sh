2d2f767dba46fc83a884f08033ff8d0b523f22e6
johann8 <jo.hahn@gmx.de>
2024-03-05 11:46:22 +0100

    fixed typos

--- docker/systems/alpine/entrypoint/docker-entrypoint-standalone.sh
+++ docker/systems/alpine/entrypoint/docker-entrypoint-standalone.sh
@@ -52,45 +52,51 @@ echo "[done]"
 
 ### control bacula config
 if [ ! -f /etc/bacula/bacula-config.control ]; then
-  tar xzf /bacula-dir.tgz --backup=simple --suffix=.before-control
+   tar xzf /bacula-dir.tgz --backup=simple --suffix=.before-control
 
-  ### === Mail delivery ===
-  if [ ! -z ${SMTP_HOST} ]; then
-     # hostname & port 
-     echo -n "Setting mail hostname & port...          "
-     sed -i -e "s/-h localhost/-h ${SMTP_HOST}/g" ${BACULA_DIR_CONFIG}
-     echo "[done]"
-  fi
+   ### === Mail delivery ===
+   if [ ! -z ${SMTP_HOST} ]; then
+      # hostname & port 
+      echo -n "Setting mail hostname & port...          "
+      sed -i -e "s/-h localhost/-h ${SMTP_HOST}/g" ${BACULA_DIR_CONFIG}
+      echo "[done]"
+   fi
 
-  # admin mail addresss
-  if [ ! -z ${ADMIN_MAIL} ]; then
-     echo -n "Setting admin user mail address...       "
-     sed -i -e "s/mail = root@localhost/mail = ${ADMIN_MAIL}/g" \
-            -e "s/operator = root@localhost/operator = ${ADMIN_MAIL}/g" ${BACULA_DIR_CONFIG}
-     echo "[done]"
-  fi
+   # admin mail addresss
+   if [ ! -z ${ADMIN_MAIL} ]; then
+      echo -n "Setting admin user mail address...       "
+      sed -i -e "s/mail = root@localhost/mail = ${ADMIN_MAIL}/g" \
+             -e "s/operator = root@localhost/operator = ${ADMIN_MAIL}/g" ${BACULA_DIR_CONFIG}
+      echo "[done]"
+   fi
 
-  # Change bacula-dir daemon name
-  if [ ! -z ${BUILD_DAEMON_NAME} ]; then
-     echo -n "Setting daemon names...                  "
-     sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_DIR_CONFIG}
-     sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_FD_CONFIG}
-     sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_SD_CONFIG}
-     sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BCONSOLE_CONFIG}
-     echo "[done]"
-  fi
+#  # Change Address of Autochanger, bacula-fd, bconsole
+#  echo -n "Changing IP address to localhost...      "
+#  sed -i -e "s/^  Address = ${LOCAL_ADDRESS}/  Address = localhost/" ${BACULA_DIR_CONFIG}
+#  echo "[done]"
 
-  # Delete old pools; add storage pools: Full, Differential, Incremental
-  if [[ "${ADD_STORAGE_POOL}" = true ]]; then
-     # Delete old storage pools
-     echo -n "Deleting old storage pools...            "
-     sed -i -e '/# Default pool definition/,+10d' \
-       -e '/# File Pool definition/,+11d' ${BACULA_DIR_CONFIG}
-     echo "[done]"
+   # Change bacula-dir daemon name (build-3-19-x86_64)
+   if [ ! -z ${BUILD_DAEMON_NAME} ]; then
+      echo -n "Setting daemon names...                  "
+      sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_DIR_CONFIG}
+      sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_FD_CONFIG}
+      sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BACULA_SD_CONFIG}
+      sed -i "s/${BUILD_DAEMON_NAME}/${DESIRED_DAEMON_NAME}/g" ${BCONSOLE_CONFIG}
+      #sed -i "s/address = ${DESIRED_DAEMON_NAME}/address = localhost/" ${BCONSOLE_CONFIG}
+      echo "[done]"
+   fi
 
-     # Add storage pools: Full, Differential, Incremental
-     echo -n "Creating storage pools...                "
-     cat >> ${BACULA_DIR_CONFIG} << 'EOL'
+   # Delete old pools; add storage pools: Full, Differential, Incremental
+   if [[ "${ADD_STORAGE_POOL}" = true ]]; then
+      # Delete old storage pools
+      echo -n "Deleting old storage pools...            "
+      sed -i -e '/# Default pool definition/,+10d' \
+             -e '/# File Pool definition/,+11d' ${BACULA_DIR_CONFIG}
+      echo "[done]"
+
+      # Add storage pools: Full, Differential, Incremental
+      echo -n "Creating storage pools...                "
+      cat >> ${BACULA_DIR_CONFIG} << 'EOL'
 Pool {
   Name = "Differential"
   Description = "Differential Pool"
@@ -133,42 +139,41 @@ Pool {
   Recycle = yes
 }
 EOL
-     echo "[done]"
+      echo "[done]"
 
-     # Change docker bacula client
-     echo -n "Changing backup job name...              "
-     sed -i -e 's/Name = "BackupClient1"/Name = "backup-bacula-fd"/g' \
-            -e 's/Pool = File/Pool = "Incremental"/g' \
-            -e 's/Full Set/bacula-fd-fset/g' ${BACULA_DIR_CONFIG}
-     echo "[done]"
+      # Change docker bacula client
+      echo -n "Changing backup job name...              "
+      sed -i -e 's/Name = "BackupClient1"/Name = "backup-bacula-fd"/g' \
+             -e 's/Pool = File/Pool = "Incremental"/g' \
+             -e 's/Full Set/bacula-fd-fset/g' ${BACULA_DIR_CONFIG}
+      echo "[done]"
 
-     echo -n "Changing backup job description...       "     
-     sed -i -e '/  Name = "backup-bacula-fd"/a\  Description = "Backup bacula docker container"' ${BACULA_DIR_CONFIG}
-     echo "[done]"
+      echo -n "Changing backup job description...       "     
+      sed -i -e '/  Name = "backup-bacula-fd"/a\  Description = "Backup bacula docker container"' ${BACULA_DIR_CONFIG}
+      echo "[done]"
 
-     echo -n "Adding storage pools to jobdefs...       "
-     sed -i -e '/  Name = "DefaultJob"/a\  DifferentialBackupPool = "Differential"' \
-            -e '/  Name = "DefaultJob"/a\  IncrementalBackupPool = "Incremental"' \
-            -e '/  Name = "DefaultJob"/a\  FullBackupPool = "Full"' ${BACULA_DIR_CONFIG}
-     echo "[done]"
+      echo -n "Adding storage pools to jobdefs...       "
+      sed -i -e '/  Name = "DefaultJob"/a\  DifferentialBackupPool = "Differential"' \
+             -e '/  Name = "DefaultJob"/a\  IncrementalBackupPool = "Incremental"' \
+             -e '/  Name = "DefaultJob"/a\  FullBackupPool = "Full"' ${BACULA_DIR_CONFIG}
+      echo "[done]"
 
-     #echo -n "Adding folder to fileset...              "
-     #sed -i -e '/    File = \/usr\/sbin/a\    File = /var/www/bacularis' \
-     #       -e '/    File = \/usr\/sbin/a\    File = /etc/bacula' ${BACULA_DIR_CONFIG}
-     #echo "[done]"
-  fi
+      echo -n "Changing DefaultJob name...              "
+      sed -i -e 's/^  Name = "DefaultJob"/  Name = "bacula-fd-job"/' -e 's/^  JobDefs = "DefaultJob"/  JobDefs = "bacula-fd-job"/' ${BACULA_DIR_CONFIG}
+      echo "[done]"
+   fi
 
-  # Delete bacula-fd old fileset
-  echo -n "Deleting bacula-fd old fileset...        "
-  sed -i -e '/# List of files to be backed up/,+38d' ${BACULA_DIR_CONFIG}
-  echo "[done]"
+   # Delete bacula-fd old fileset
+   echo -n "Deleting bacula-fd old fileset...        "
+   sed -i -e '/# List of files to be backed up/,+38d' ${BACULA_DIR_CONFIG}
+   echo "[done]"
 
-  # Add bacula-fd new fileset
-  echo -n "Creating bacula-fd new fileset...        "
-  cat >> ${BACULA_DIR_CONFIG} << EOL
+   # Add bacula-fd new fileset
+   echo -n "Creating bacula-fd new fileset...        "
+   cat >> ${BACULA_DIR_CONFIG} << EOL
 
 Fileset {
-  Name = "${DESIRED_DAEMON_NAME}-fd-fset"
+  Name = "${DESIRED_DAEMON_NAME}-fd-fs"
   Include {
     Options {
       Compression = "Lzo"
@@ -190,45 +195,44 @@ Fileset {
   }
 }
 EOL
-  echo "[done]"
+   echo "[done]"
 
-  #
-  #echo -n "Setting bacula config permissions...     "
-  #chown -R bacula:bacula /etc/bacula/*
-  #chown bacula:tape /opt/bacula/archive
-  #chmod -R 775 /etc/bacula/*
-  #echo "[done]"
+   #
+   echo -n "Setting bacula config permissions...     "
+   #chown -R bacula:bacula /etc/bacula/*
+   chown bacula:tape /var/lib/bacula/archive
+   #chmod -R 775 /etc/bacula/*
+   echo "[done]"
 
-  # Set bacula-sd ip address
-  if [ ! -z ${DOCKER_HOST_IP} ]; then
-     # Storage File1
-     n1=
-     echo -n "Setting Storage \"File1\" IP address...    "
-     n1=$(cat ${BACULA_DIR_CONFIG} |grep -niw 'Name = File1' | awk -F: '{ print $1 }')
-     n1=$(($n1+2))
-     sed -i -e "${n1}s+localhost+${DOCKER_HOST_IP}+" ${BACULA_DIR_CONFIG}
-     echo "[done]"
+   # Set bacula-sd ip address
+   if [ ! -z ${DOCKER_HOST_IP} ]; then
+      # Storage File1
+      n1=
+      echo -n "Setting Storage \"File1\" IP address...    "
+      n1=$(cat ${BACULA_DIR_CONFIG} |grep -niw 'Name = File1' | awk -F: '{ print $1 }')
+      n1=$(($n1+2))
+      sed -i -e "${n1}s+localhost+${DOCKER_HOST_IP}+" ${BACULA_DIR_CONFIG}
+      echo "[done]"
 
-     # Storage File2
-     echo -n "Setting Storage \"File2\" IP address...    "
-     n2=
-     n2=$(cat ${BACULA_DIR_CONFIG} |grep -niw 'Name = File2' | awk -F: '{ print $1 }')
-     n2=$(($n2+2))
-     sed -i -e "${n2}s+localhost+${DOCKER_HOST_IP}+" ${BACULA_DIR_CONFIG}
-     echo "[done]"
-  fi
+      # Storage File2
+      echo -n "Setting Storage \"File2\" IP address...    "
+      n2=
+      n2=$(cat ${BACULA_DIR_CONFIG} |grep -niw 'Name = File2' | awk -F: '{ print $1 }')
+      n2=$(($n2+2))
+      sed -i -e "${n2}s+localhost+${DOCKER_HOST_IP}+" ${BACULA_DIR_CONFIG}
+      echo "[done]"
+   fi
 
-
-  # Control file
-  touch /etc/bacula/bacula-config.control
+   # Control file
+   touch /etc/bacula/bacula-config.control
 fi
 
 ### Control bacula storage 
 if [ ! -f /var/lib/bacula/bacula-sd.control ]; then
-  tar xzf /bacula-sd.tgz --backup=simple --suffix=.before-control
+   tar xzf /bacula-sd.tgz --backup=simple --suffix=.before-control
 
-  # Control file
-  touch /var/lib/bacula/bacula-sd.control
+   # Control file
+   touch /var/lib/bacula/bacula-sd.control
 fi
 
 ### Control bacularis-app
@@ -281,51 +285,51 @@ export PGPASSWORD=${DB_ADMIN_PASSWORD}
 
 # PostgresDB helthcheck
 if [[ -z ${CI_TEST} ]] ; then
-  # Waiting Postgresql is up
-  sqlup=1
-  while [ "$sqlup" -ne 0 ] ; do
-    echo "Waiting for postgresql..."
-    pg_isready --host="${DB_HOST}" --port="${DB_PORT}" --user="${DB_ADMIN_USER}"
-    if [ $? -ne 0 ] ; then
-      sqlup=1
-      sleep 5
-    else
-      sqlup=0
-      echo "...postgresql is alive"
-    fi
-  done
+   # Waiting Postgresql is up
+   sqlup=1
+   while [ "$sqlup" -ne 0 ] ; do
+      echo "Waiting for postgresql..."
+      pg_isready --host="${DB_HOST}" --port="${DB_PORT}" --user="${DB_ADMIN_USER}"
+      if [ $? -ne 0 ] ; then
+         sqlup=1
+         sleep 5
+      else
+         sqlup=0
+         echo "...postgresql is alive"
+      fi
+   done
 fi
 
 # Init PostgresDB
 if [ ! -f /etc/bacula/bacula-db.control ] && [ "${DB_INIT}" == 'true' ] ; then
 
-  # Add pgpass file to ${DB_USER} home
-  homedir=$(getent passwd "$DB_USER" | cut -d: -f6)
-  echo "${DB_HOST}:${DB_PORT}:${DB_NAME}:${DB_USER}:${DB_PASSWORD}" > "${homedir}/.pgpass"
-  chmod 600 "${homedir}/.pgpass"
-  chown "${DB_USER}" "${homedir}/.pgpass"
+   # Add pgpass file to ${DB_USER} home
+   homedir=$(getent passwd "$DB_USER" | cut -d: -f6)
+   echo "${DB_HOST}:${DB_PORT}:${DB_NAME}:${DB_USER}:${DB_PASSWORD}" > "${homedir}/.pgpass"
+   chmod 600 "${homedir}/.pgpass"
+   chown "${DB_USER}" "${homedir}/.pgpass"
 
-  # Init Postgres DB
-  echo "*** Bacula DB init ***"
-  echo "Bacula DB init: Create user ${DB_USER}"
-  psql -c "create user ${DB_USER} with createdb createrole login;"
-  echo "Bacula DB init: Set user password"
-  psql -c "alter user ${DB_USER} password '${DB_PASSWORD}';"
-  /etc/bacula/scripts/create_bacula_database 2>/dev/null
-  /etc/bacula/scripts/make_bacula_tables  2>/dev/null
-  /etc/bacula/scripts/grant_bacula_privileges  2>/dev/null
-  # Control file
-  touch /etc/bacula/bacula-db.control
+   # Init Postgres DB
+   echo "*** Bacula DB init ***"
+   echo "Bacula DB init: Create user ${DB_USER}"
+   psql -c "create user ${DB_USER} with createdb createrole login;"
+   echo "Bacula DB init: Set user password"
+   psql -c "alter user ${DB_USER} password '${DB_PASSWORD}';"
+   /etc/bacula/scripts/create_bacula_database 2>/dev/null
+   /etc/bacula/scripts/make_bacula_tables  2>/dev/null
+   /etc/bacula/scripts/grant_bacula_privileges  2>/dev/null
+   # Control file
+   touch /etc/bacula/bacula-db.control
 fi
 
 # Update Postgres DB
 if [ "${DB_UPDATE}" == 'true' ] ; then
-  # Try Postgres upgrade
-  echo "*** Bacula DB update ***"
-  echo "Bacula DB update: Update tables"
-  /etc/bacula/scripts/update_bacula_tables  2>/dev/null
-  echo "Bacula DB update: Grant privileges"
-  /etc/bacula/scripts/grant_bacula_privileges  2>/dev/null
+   # Try Postgres upgrade
+   echo "*** Bacula DB update ***"
+   echo "Bacula DB update: Update tables"
+   /etc/bacula/scripts/update_bacula_tables  2>/dev/null
+   echo "Bacula DB update: Grant privileges"
+   /etc/bacula/scripts/grant_bacula_privileges  2>/dev/null
 fi
 
 echo ""

eb8f45640bab55f814be4946dd30caa455287768
johann8 <jo.hahn@gmx.de>
2026-06-19 10:09:05 +0200

    changed docker image version to 6.3.0; small changes

--- build_image_psql17.sh
+++ build_image_psql17.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # set variables
-D_IMAGE_VERSION=6.2.1.1
-PG_VERSION=psql17
+D_IMAGE_VERSION=6.3.0
+D_IMAGE_TAG=alpine
+BASE_IMAGE=alpine:3.24
 
-# build docker image
+BACULARIS_VERSION=6.3.0
+BACULA_VERSION=15.0.3-r0
+POSTGRES_VERSION=17
+
+
+# create build docker image
+#docker build -f ./Dockerfile -t johann8/bacularis:${D_IMAGE_VERSION}-alpine . 2>&1 | tee ./build.log
 docker build \
-  --build-arg=BACULARIS_VERSION=6.2.1 \
-  --build-arg=BACULA_VERSION=15.0.3-r0 \
-  --build-arg=POSTGRES_VERSION=17 \
+  --build-arg=BASE_IMAGE=${BASE_IMAGE} \
+  --build-arg=BACULARIS_VERSION=${BACULARIS_VERSION} \
+  --build-arg=BACULA_VERSION=${BACULA_VERSION} \
+  --build-arg=POSTGRES_VERSION=${POSTGRES_VERSION} \
   --platform=linux/amd64 \
-  --tag=johann8/bacularis:${D_IMAGE_VERSION}-alpine-${PG_VERSION} \
+  --tag=johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-psql${POSTGRES_VERSION} \
   --file=./Dockerfile . 2>&1 | tee ./build.log
 
 _BUILD=$?
@@ -22,14 +30,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a
-   docker tag johann8/bacularis:${D_IMAGE_VERSION}-alpine-${PG_VERSION} johann8/bacularis:latest-alpine-${PG_VERSION}
+   docker tag johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-psql${POSTGRES_VERSION} johann8/bacularis:latest-${D_IMAGE_TAG}-psql${POSTGRES_VERSION}
 fi
 
 #push image to docker hub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
-   docker push johann8/bacularis:latest-alpine-${PG_VERSION}
-   docker push johann8/bacularis:${D_IMAGE_VERSION}-alpine-${PG_VERSION}
+   docker push johann8/bacularis:latest-${D_IMAGE_TAG}-psql${POSTGRES_VERSION}
+   docker push johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-psql${POSTGRES_VERSION}
    _PUSH=$?
    docker images -a |grep bacularis
 fi
@@ -38,9 +46,8 @@ fi
 #delete build local
 if [ ${_PUSH=} = 0 ]; then
    echo "Deleting docker images..."
-   docker rmi johann8/bacularis:latest-alpine-${PG_VERSION}
-   #docker images -a
-   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-alpine-${PG_VERSION}
+   docker rmi johann8/bacularis:latest-${D_IMAGE_TAG}-psql${POSTGRES_VERSION}
+   docker rmi johann8/bacularis:${D_IMAGE_VERSION}-${D_IMAGE_TAG}-psql${POSTGRES_VERSION}
    docker images -a
 fi
 
